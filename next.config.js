a176ae226ae247a1390fe00538c479d9e7fa7eab
Yog <yog.panjarale@gmail.com>
2021-07-19 16:18:08 +0530

    redirect  index to home

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
     pwa: {
         dest: 'public'
-    }
+    },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false,
+      },
+    ]
+  },
 })
